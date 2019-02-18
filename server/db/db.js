c4024f03a5082666f16e697cf0583025e733b451
Plataforma 5 - Coding School <contacto@plataforma5.la>
2019-02-18 15:13:17 -0300

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 const Sequelize = require('sequelize');
 const DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
 
-console.log(chalk.yellow('Opening connection to PostgreSQL\n'));
+console.log(chalk.yellow('\nOpening connection to PostgreSQL'));
 
 // create the database instance
 module.exports = new Sequelize(DATABASE_URI, {
