2f9b94e6c948d5a7140c606f2c53c135102fecaf
Plataforma 5 - Coding School <contacto@plataforma5.la>
2019-05-28 14:34:12 -0300

    Update Sidebar.jsx

--- browser/react/components/Sidebar.jsx
+++ browser/react/components/Sidebar.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default ({ deselectAlbum }) => (
   <div className="col-xs-2">
-      <sidebar>
+      <section className='sidebar'>
         <img src="juke.svg" className="logo" />
         <section>
           <h4 className="menu-item active">
             <a onClick={deselectAlbum} href="#">ALBUMS</a>
           </h4>
         </section>
-      </sidebar>
+      </section>
     </div>
 );
